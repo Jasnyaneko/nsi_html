014df5105075d05638e7bed4154e2d589043ebf5
jasnyaneko <jasnyaneko@hotmail.com>
2026-04-28 00:08:06 +0200

    game upgraded reste plus que la partie artistique entre autre graphisme sfx et musique

--- js/p2.js
+++ js/p2.js
@@ -1,85 +1,141 @@
-let score = [0,0,0,0]
-let miss = 0
+let playing = false
 
-let active_blocks = [false,false,false,false]
+let loop;
 
-let newtime_start = new Date().getTime()
-const game_started_set_time = () => {
-    return newtime_start
-}
+let game_started_time = new Date().getTime()
+//defini quand le jeu a commencé et permet de calculer le temps écoulé depuis le début du jeu
 
 let time_gap = 200
-const key1 = "a"
-const key2 = "s"
-const key3 = "d"
-const key4 = "f"
+//représente la marge de temps pour laquelle un click est considéré comme valide (en ms)
 
-let game_started_time = game_started_set_time()
-const partition = {
-    /* touche:[temps] */
-    "key1":[760,760*2],
-    "key2":[760,760*3],
-    "key3":[760*2,760*4],
-    "key4":[760*3,760*4]
-}
+let now;
 
-let iter1 = 0
-let iter2 = 0
-let iter3 = 0
-let iter4 = 0
-
-const rythmloop = () => {setInterval(() => {
-    let now = new Date().getTime() - game_started_time
-    active_blocks = [false,false,false,false]
-            if(partition["key1"][iter1] < (now - time_gap) && partition["key1"][iter1] <= partition["key1"][iter1]){
-                iter1 = iter1 + 1
-                active_blocks[0] = true
-                console.log(key1)
-            }
-            if(partition["key2"][iter2] < (now - time_gap) ){
-                iter2 = iter2 + 1
-                active_blocks[1] = true
-                console.log(key2)
-            }
-            if(partition["key3"][iter3] < (now - time_gap) ){
-                iter3 = iter3 + 1
-                active_blocks[2] = true
-                console.log(key3)
-            }
-            if(partition["key4"][iter4] < (now - time_gap) ){
-                iter4 = iter4 + 1
-                active_blocks[3] = true
-                console.log(key4)
-            }
-    },500)}
-
-document.body.addEventListener("keydown",(key) => {
-    console.log(active_blocks)
-switch (key.key.toString()){
-    case "a":
-        if(active_blocks[0] == true && score[0] < iter1){
-            score[0] = score[0] + 1
-        }
-    break;
-    case key2:
-        if(active_blocks[1] == true && score[1] < iter2){
-            score[1] = score[1] + 1
-        }
-    break;
-    case key3:
-        if(active_blocks[2] == true && score[2] < iter3){
-            score[2] = score[2] + 1
-        }
-    break;
-    case key4:
-        if(active_blocks[3] == true && score[3] < iter4){
-            console.log(score)
-            score[3] = score[3] + 1
-        }
-    break;
-}
+let init_part = () => ({
+//part pour patrition, représente les éléments du jeu de rythme , les listes contiennent toutes quatres variables correspondant respectivement a une case
+    totalnote:0,
+    totalscore:0,
+    timestamps:[[760,760*2],[760,760*3,760*5,760*6],[760*2,760*4],[760*3,760*4]],
+    //liste de liste contenant les différents moments où les chaque note doit être appuyé
+    iterator:[0,0,0,0],
+    key:["a","s","d","f"],
+    //on peut changer les touches de jeux ici
+    pressed:[new Set(),new Set(),new Set(),new Set()],
+    //représente le nombre de fois qu'une case a été jouée , l'usage de sets permet d'éviter les problemes de doubles cliques dans le jeu ceux ci ne préservant qu'une fois chaque valeurs
+    miss:[0,0,0,0],
+    //représente les cases ratées
+    active_blocks:[false,false,false,false],
+    finished:[false,false,false,false]
+    //sert a dire si une case peux etre joué ou pas
 })
 
-/* il reste juste à ajouter un délai avant de pouvoir reclicker (pour l'instant il est possible de cliquer deux fois sur la derniere occurence d'une touche pour compléter son score entierement) */
+let part = init_part()
 
-rythmloop()
+const rythmloop = () => {
+    // représente la partie répété du jeu soit casiment tout le jeu en fait
+
+    now = new Date().getTime() - game_started_time
+    //donne le temps écoulé depuis le début du jeu et sert a estimer ou le joueur est dans le jeu
+
+    part.active_blocks = [false,false,false,false]
+    // redéfini les actives_blocks pour que les cases non actives soit mise a jour
+    console.log("loop")
+    for (let i = 0 ; i < 4 ; i = i + 1){
+        // l'iterateur ici représente les cases
+        if(part.timestamps[i][part.iterator[i]] >= (now - time_gap) &&  part.timestamps[i][part.iterator[i]] <= (now + time_gap)
+        /* regarde si la note actuelle se trouve dans la zone d'action */){
+            part.active_blocks[i] = true
+            /*console.log(part.key[i])*/
+        }
+        else if(part.timestamps[i][part.iterator[i]] < (now - time_gap)){
+            // regarde si la note est passé auquel cas l'iterateur de la case passe a la prochaine case
+            part.miss[i] = part.miss[i] + 1
+            part.iterator[i] = part.iterator[i] + 1
+            part.totalnote = part.totalnote + 1
+        }
+    
+        if(part.iterator[i] >= (part.timestamps[i].length) ){
+            part.finished[i] = true
+        }
+
+        if(part.active_blocks[i]){
+            //defini le style des cases actives
+            document.getElementById("k" + i.toString()).style.color = "green"
+        }
+        else if((part.timestamps[i][part.iterator[i]] - (now + time_gap)) < 400 && (part.timestamps[i][part.iterator[i]] - (now + time_gap)) > 0){
+            document.getElementById("k" + i.toString()).style.color = "yellow"
+        }
+        else{
+            // defini le style des cases inactive
+            document.getElementById("k" + i.toString()).style.color = "red"
+        }
+    }   
+    if(part.finished.every((cases) => {
+        return (cases === true)
+    })){
+        part.playing = false
+        cancelAnimationFrame(loop)
+        return;
+    }
+    loop = requestAnimationFrame(rythmloop)
+    //loop est egale a lidentificateur le boucle et par la fonction requestAnimationFrame répeter la boucle
+}
+
+    //loop = requestAnimationFrame(rythmloop)
+    // sert a récupérer l'Id de la boucle en dehors de celle ci
+
+    
+    //arrete la boucle
+
+document.body.addEventListener("keydown",(key) => {
+    // le callback spécifie la touche appuyée
+
+    /*console.log(part.active_blocks)*/
+
+    for(let i = 0 ; i < 4 ; i = i + 1){
+        //itere a travers les cases
+
+        if(key.key.toString() == part.key[i] && part.active_blocks[i] == true){
+            //vérifie si la touche appuyé correspond a la case
+
+            if(!part.pressed[i].has(part.iterator[i])){
+                // verifie que la note n'a pas deja été jouée (par absence dans le set)
+
+                if(Math.abs(part.timestamps[i][part.iterator[i]] - now) < 50){
+                    console.log("perfect")
+                    part.totalscore = part.totalscore + 2
+                }
+                else{
+                part.totalscore = part.totalscore + 1
+                }
+                part.pressed[i].add(part.iterator[i])
+                // ajoute la note au set pour qu'elle ne soit plus jouable
+                
+            }
+        part.active_blocks[i] = false
+        }
+    }
+    if(key.key.toString() == "z"){
+        console.log("missed ",part.totalnote - part.totalscore, "\n score",part.totalscore)
+    }
+})
+
+document.getElementById("start_stop").addEventListener("click",()=>{
+    if(playing == false){
+        part = init_part()
+        game_started_time = new Date().getTime()
+        console.log("yyyyy")
+        playing = true
+        loop = requestAnimationFrame(rythmloop)
+    }
+    else if(playing == true ){
+        console.log(part)
+        playing = false
+
+        cancelAnimationFrame(loop)
+    }
+    else{console.log("il y a un gros probleme")}
+    
+})
+
+
+/* peut être mettre des graphismes nn ? XD */
